b90a40a45c140e0a6381f9202871ef26c21693e4
Kim sungsoo <tjdtnsla0911@naver.com>
2020-08-24 11:36:03 +0800

    아직 테스트중입니다

---
+++ src/main/java/com/aruerue/shop/dto/addminDto/AddminDto.java
@@ -0,0 +1,36 @@
+package com.aruerue.shop.dto.addminDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddminDto {
+private int id;
+private String title;
+private String thumb;
+private int price;
+private String disc;
+private int discounted;
+private String content;
+private String bgImg;
+private String radioSale;
+private String radioAd;
+private String radioParentTypeId; //카테고리아이디 겸용
+private String radioBest;
+private String radioNew;
+//html + ajax에서 boolean형으로 true 적어서 문자형으로 자꾸인식해서 
+//위에서 String 형으로 한번받고 if으로 다시 boolean으로 하는 방식 택했습니다.
+private boolean radioSale2;
+private boolean radioAd2;
+private boolean radioParentTypeId2; //카테고리아이디 겸용
+private boolean radioBest2;
+private boolean radioNew2;
+
+
+
+}
