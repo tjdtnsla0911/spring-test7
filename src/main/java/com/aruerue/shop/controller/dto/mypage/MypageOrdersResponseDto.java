7a1b637f6458492a1fcdc43f055a692352604d23
Moonseonhyeon <msh1730@gmail.com>
2020-08-26 18:25:28 +0900

    dto만들기 시작

---
+++ src/main/java/com/aruerue/shop/controller/dto/mypage/MypageOrdersResponseDto.java
@@ -0,0 +1,27 @@
+package com.aruerue.shop.controller.dto.mypage;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MypageOrdersResponseDto {
+	
+	// o.id, o.orderDate, o.totalPrice, o.howToPay, od.stats, p.title, o.userId
+	private int id;
+	private Timestamp orderDate;
+	private int totalPrice;
+	private String howToPay;
+	private String stats;
+	private String title;
+	private int userId;
+	
+
+
+}
