cc4bf618ba0da5abe5934f39da08c65c327a4d76
Linda <msh1730@gmail.com>
2020-08-28 00:37:56 +0900

    마이페이지 취소/교환/반품 Dto추가, README파일 테이블+ 더미데이터 수정

---
+++ src/main/java/com/aruerue/shop/controller/dto/mypage/MypageCancleRespDto.java
@@ -0,0 +1,14 @@
+package com.aruerue.shop.controller.dto.mypage;
+
+
+// od.orderId, od.productId, od.quantity, od.stats, p.title, p.thumb
+public class MypageCancleRespDto {
+	private int orderId;
+	private int productId;
+	private String quantity; //지우고 싶음...
+	private String stats;
+	private String title;
+	private String thumb;
+	
+	
+}
