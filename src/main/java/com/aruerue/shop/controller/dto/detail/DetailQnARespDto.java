22e46fa80ee2702d9c7e578e9eb937db9b99202c
Linda <msh1730@gmail.com>
2020-08-28 09:12:33 +0900

    DetailQnARespDto 타입 수정

--- src/main/java/com/aruerue/shop/controller/dto/detail/DetailQnARespDto.java
+++ src/main/java/com/aruerue/shop/controller/dto/detail/DetailQnARespDto.java
@@ -4,7 +4,7 @@ import java.sql.Timestamp;
 
 //q.answer, q.title, u.username, q.createDate, p.id
 public class DetailQnARespDto {
-	private String answer;
+	private boolean answer;
 	private String title;
 	private String username;
 	private Timestamp createDate;
